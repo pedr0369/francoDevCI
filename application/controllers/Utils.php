87340503f6268fea2591fe48ddbe0b1530cca137
pedr369 <pedr0369@hotmail.com>
2016-10-03 15:00:12 -0300

    Primeiro migration

---
+++ application/controllers/Utils.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Utils extends CI_Controller {
+
+    public function migrate(){
+        $this->load->library('migration');
+        $success = $this->migration->current();
+        if($success){
+            echo "<script>alert('Executado com sucesso');</script>";
+        }
+        else{
+            show_error($this->migration->error_string());
+        }
+    }
+
+}
